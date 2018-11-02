f345c314767024148b4aa7ccd61d366205cfc088
dsimon01 <dsimon01@dcs.bbk.ac.uk>
2018-11-02 20:37:43 +0000

    Login and Home design improvements

--- app/src/main/java/com/gbk/simoni/gbk/LoginActivity.java
+++ app/src/main/java/com/gbk/simoni/gbk/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
