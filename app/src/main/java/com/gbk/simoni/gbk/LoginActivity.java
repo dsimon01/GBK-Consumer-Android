6b0251221f2c0d0d09052d381d6239fa651cf6a2
dsimon01 <dsimon01@dcs.bbk.ac.uk>
2019-01-03 19:13:34 +0000

    login keybord handling for a more user friendly interactions

--- app/src/main/java/com/gbk/simoni/gbk/LoginActivity.java
+++ app/src/main/java/com/gbk/simoni/gbk/LoginActivity.java
@@ -1,37 +1,66 @@
 package com.gbk.simoni.gbk;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnKeyListener, View.OnClickListener {
+
+    EditText username;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.usernameEditText);
+        password = findViewById(R.id.passwordEditText);
+        password.setOnKeyListener(this);
+        ConstraintLayout background  = findViewById(R.id.backgroundLayout);
+        background.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+        if (keyCode == event.KEYCODE_ENTER && event.getAction() == event.ACTION_DOWN) {
+            onLoginClick(v);
+        }
+        return false;
+    }
+
+    public void onClick(View view){
+        if (view.getId() == R.id.backgroundLayout || view.getId() == R.id.loginImageView){
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
 
     //When the user selects the login addToBasketButton:
-    public void onLoginClick(View view){
-
-        //locate their input for username and password and assign them to corresponding vars.
-        final EditText username = findViewById(R.id.usernameEditText);
-        EditText password = findViewById(R.id.passwordEditText);
+    public void onLoginClick(View view) {
 
         //error handling for the incorrect input.
-        if (username.getText().toString().matches("") || password.getText().toString().matches("")){
+        if (username.getText().toString().matches("") || password.getText().toString().matches("")) {
             Toast.makeText(this, "Username/Password required", Toast.LENGTH_SHORT).show();
-        }else {
+        } else {
             //validate the user input with the credentials stored in the server and attempt to login.
             //upon successful login, redirects the user to the Home activity
             ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
                 @Override
                 public void done(ParseUser user, ParseException e) {
-                    if (user != null){
+                    if (user != null) {
                         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                         startActivity(intent);
-                    }else {
+                    } else {
                         //if login fails show error in Toast.
                         Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
@@ -39,10 +68,5 @@ public class LoginActivity extends AppCompatActivity {
             });
         }
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
 }
+
--- app/src/main/java/com/gbk/simoni/gbk/OrderUpdatesActivity.java
+++ app/src/main/java/com/gbk/simoni/gbk/OrderUpdatesActivity.java
@@ -2,6 +2,8 @@ package com.gbk.simoni.gbk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,11 +13,19 @@ public class OrderUpdatesActivity extends AppCompatActivity {
 
 // Activity for order updates:
 
+    RecyclerView basketRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_updates);
 
+        basketRecyclerView = findViewById(R.id.recyclerViewBasket);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        basketRecyclerView.setLayoutManager(linearLayoutManager);
+        basketRecyclerView.setHasFixedSize(true);
+        basketRecyclerView.setAdapter(new BasketAdapter(MenuActivity.selectedItemsList));
 
     }
 }
